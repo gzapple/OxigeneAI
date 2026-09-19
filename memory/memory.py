b28666492de4a68d31e37a46b0b5d01521e8b49c
GZAPPLE <supergodzillapple@gmail.com>
2026-09-19 12:53:53 -0400

    initialized memory bank for bare minimum stuff

--- memory/memory.py
+++ memory/memory.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+# memory.py  - responsible of storing and retrieving the bot's memory, such as the last X strings read from a channel.
+
+class ServerMemory:
+
+    def __init__(self):
+        self.tokens = [] 
+        self.max_tokens = 1000
+        # this is a constructor responsible to create an amount of tokens
+
+    def add_message(self, message):
+        new_tokens = message.content.split()
+
+        for token in new_tokens:
+            self.tokens.append(token)
+
+    def get_memory(self):
+        return self.tokens
